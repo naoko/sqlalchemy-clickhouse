f8c779cd9494d47f408bd13142abfea0d05e9864
Naoko <naoko.reeves@gmail.com>
2020-11-13 08:11:21 -0700

    ADD LowCardinality(FixedString(3)) case

--- connector.py
+++ connector.py
@@ -90,6 +90,9 @@ def create_ad_hoc_field(cls, db_type):
 
     if db_type == 'LowCardinality(Nullable(FixedString(3)))':
         db_type = 'String'
+    
+    if db_type == 'LowCardinality(FixedString(3))':
+        db_type = 'String'
 
     if db_type.startswith('DateTime'):
         db_type = 'DateTime'
